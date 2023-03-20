0053f9924646510263067f0ef5a29020fb081d49
Ilham Hadi Prabawa <ilhamprabawa9@gmail.com>
2023-03-20 12:55:36 +0700

    added router destination

--- Twier/Features/Post/View/AllPostView.swift
+++ Twier/Features/Post/View/AllPostView.swift
@@ -7,21 +7,41 @@ import SwiftUI
 
 struct AllPostView: View {
   @Binding var items: [Post]
+  var presenter: TwierPresenter
   
   var body: some View {
-    List(items) { post in
-      RowView(name: post.user?.name ?? "",
-              username: post.user?.username ?? "",
-              text: post.message ?? "",
-              imageData: post.image)
-      .padding(.top, 16)
+    ScrollView {
+      VStack {
+        HStack {
+          Spacer()
+          presenter.seeAllLinkBuilder {
+            Text("See All")
+              .font(.headline)
+              .fontWeight(.medium)
+              .foregroundColor(Color.gray)
+          }
+        }
+        .padding(.trailing, 15)
+      }
+      
+      LazyVStack(alignment: .leading) {
+        ForEach(items) { post in
+          presenter.detailLinkBuilder {
+            RowView(name: post.user?.name ?? "",
+                    username: post.user?.username ?? "",
+                    text: post.message ?? "",
+                    imageData: post.image)
+            .padding(.top, 16)
+          }
+        }
+      }
     }
-    .listStyle(.plain)
   }
 }
 
 struct AllPostView_Previews: PreviewProvider {
   static var previews: some View {
-    AllPostView(items: .constant([]))
+    AllPostView(items: .constant([]),
+                presenter: TwierPresenter(interactor: DependencyInjection.shared.provideTwierInteractor(), userInteractor: DependencyInjection.shared.provideUserInteractor(), router: TwierRouter(), userSession: UserSession()))
   }
 }
--- Twier/Features/Post/View/MyPostView.swift
+++ Twier/Features/Post/View/MyPostView.swift
@@ -9,22 +9,43 @@ import SwiftUI
 struct MyPostView: View {
   
   @Binding var items: [Post]
+  var presenter: TwierPresenter
   
   var body: some View {
-    List(items) { post in
-      RowView(name: post.user?.name ?? "",
-              username: post.user?.username ?? "",
-              text: post.message ?? "",
-              imageData: post.image)
-      .padding(.top, 16)
+    ScrollView {
+      VStack {
+        HStack {
+          Spacer()
+          presenter.seeAllLinkBuilder {
+            Text("See All")
+              .font(.headline)
+              .fontWeight(.medium)
+              .foregroundColor(Color.gray)
+          }
+        }
+        .padding(.trailing, 15)
+      }
+      
+      LazyVStack(alignment: .leading) {
+        ForEach(items) { post in
+          presenter.detailLinkBuilder {
+            RowView(name: post.user?.name ?? "",
+                    username: post.user?.username ?? "",
+                    text: post.message ?? "",
+                    imageData: post.image)
+            .padding(.top, 16)
+          }
+        }
+      }
     }
-    .listStyle(.plain)
+    
   }
 }
 
 struct MyPostView_Previews: PreviewProvider {
   static var previews: some View {
-    MyPostView(items: .constant([]))
+    MyPostView(items: .constant([]),
+               presenter: TwierPresenter(interactor: DependencyInjection.shared.provideTwierInteractor(), userInteractor: DependencyInjection.shared.provideUserInteractor(), router: TwierRouter(), userSession: UserSession()))
   }
 }
 
